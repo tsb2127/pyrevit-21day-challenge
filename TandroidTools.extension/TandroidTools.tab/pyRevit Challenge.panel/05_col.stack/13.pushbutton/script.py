486a5dc36ad4ddac7ede2b1640769bb6103f1cfa
Tanmay Bhalerao <tsb2127@gmail.com>
2026-03-15 22:19:15 -0500

    improve(day13): Floorify Refactor

--- TandroidTools.extension/TandroidTools.tab/pyRevit Challenge.panel/05_col.stack/13.pushbutton/script.py
+++ TandroidTools.extension/TandroidTools.tab/pyRevit Challenge.panel/05_col.stack/13.pushbutton/script.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __title__   = "13 - Floorify"
-__doc__     = """Version = 1.0
+__doc__     = """Version = 2.0
 Date    = 03.15.2026
 ________________________________________________________________
 Description:
@@ -8,16 +8,19 @@ Creates floors automatically from room boundaries.
 
 ________________________________________________________________
 How-To:
-1. Run the tool from the pyRevit ribbon
-2. Select a room in the model
-3. The script reads the room boundary
-4. A floor is automatically created matching the room outline
-5. The created floor is printed in the pyRevit output window
+1. User selects rooms
+2. User selects floor type
+3. Script extracts room boundary geometry
+4. Boundary curves are converted to CurveLoops
+5. Floors are generated on the same level
+6. Offset is applied
+7. A report is generated in pyRevit output
 
 ________________________________________________________________
 Last Updates:
-- [01.01.2026] v1 Proof of Concept
-________________________________________________________________
+- [01.01.2026] v2 Refactored
+_- [01.01.2026] v1 Proof of Concept
+_______________________________________________________________
 Author: Tanmay Bhalerao (Template by Erik Frits (from LearnRevitAPI.com))"""
 
 # ╦╔╦╗╔═╗╔═╗╦═╗╔╦╗╔═╗
@@ -45,46 +48,76 @@ app    = __revit__.Application
 output = script.get_output()                 # pyRevit Output Menu
 
 
-# ╔═╗╦═╗╔═╗╔═╗╔═╗  ╔═╗╔═╗  ╔═╗╔═╗╔╗╔╔═╗╔═╗╔═╗╔╦╗
-# ╠═╝╠╦╝║ ║║ ║╠╣   ║ ║╠╣   ║  ║ ║║║║║  ║╣ ╠═╝ ║
-# ╩  ╩╚═╚═╝╚═╝╚    ╚═╝╚    ╚═╝╚═╝╝╚╝╚═╝╚═╝╩   ╩
+# ╦═╗╔═╗╔═╗╔═╗╔═╗╔╦╗╔═╗╦═╗╔═╗╔╦╗  ╔═╗╔═╗╔╦╗╔═╗
+# ╠╦╝║╣ ╠╣ ╠═╣║   ║ ║ ║╠╦╝║╣  ║║  ║  ║ ║ ║║║╣
+# ╩╚═╚═╝╚  ╩ ╩╚═╝ ╩ ╚═╝╩╚═╚═╝═╩╝  ╚═╝╚═╝═╩╝╚═╝
 #░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 
-#1️⃣ Select Room
+# 🔓 Transaction To Allow Changes
+t = Transaction(doc, 'Floorify')
+t.Start()  # 🔓
+
+
+#1️⃣ Select Rooms
 from pyrevit import revit
-room = revit.pick_element_by_category(BuiltInCategory.OST_Rooms)
+rooms = revit.pick_elements_by_category(BuiltInCategory.OST_Rooms)
 
 #2️⃣ Select FloorType
-floor_type_id = doc.GetDefaultElementTypeId(ElementTypeGroup.FloorType)
+# floor_type_id = doc.GetDefaultElementTypeId(ElementTypeGroup.FloorType)
+floor_types   = FilteredElementCollector(doc).OfClass(FloorType).ToElements()
+dict_floors   = {Element.Name.GetValue(ft) : ft for ft in floor_types}
+selection     = forms.SelectFromList.show(dict_floors.keys(), button_name='Select Floor Type', title='Floorify My Rooms')
+floor_type    = dict_floors[selection]
+floor_type_id = floor_type.Id
 
-#3️⃣ Get Room Outline
-opts            = SpatialElementBoundaryOptions()
-room_boundaries = room.GetBoundarySegments(opts)
+table = []
 
-#4️⃣ Prepare List[CurveLoop]
-# Room -> Boundary -> Segment -> Curve -> CurveLoop -> List[CurveLoop]
-curve_loops = []
-for boundary in room_boundaries:
-    curve_loop = CurveLoop()
-    for seg in boundary:
-        curve = seg.GetCurve()
-        curve_loop.Append(curve)
-    curve_loops.append(curve_loop)
+for room in rooms:
+    opts = SpatialElementBoundaryOptions()
+    opts.SpatialElementBoundaryLocation = SpatialElementBoundaryLocation.Finish #Choose from (CoreBoundary, CoreCenter, Center, Finish)
+    room_boundaries = room.GetBoundarySegments(opts)
 
-List_curve_loops = List[CurveLoop](curve_loops)
+    #4️⃣ Prepare List[CurveLoop]
+    # Room -> Boundary -> Segment -> Curve -> CurveLoop -> List[CurveLoop]
+    curve_loops = []
+    for boundary in room_boundaries:
+        curve_loop = CurveLoop()
+        for seg in boundary:
+            curve = seg.GetCurve()
+            curve_loop.Append(curve)
+        curve_loops.append(curve_loop)
 
-#🔓 Transaction To Allow Changes
-t = Transaction(doc, 'Floorify')
-t.Start()   # 🔓
+    List_curve_loops = List[CurveLoop](curve_loops)
 
-#5️⃣ Create Floor (Different in API 21/22)
-level_id = room.LevelId
-new_floor = Floor.Create(doc, List_curve_loops, floor_type_id, level_id)
 
-#6️⃣ Create Report
-link_floor = output.linkify(new_floor.Id, new_floor.Name)
-print(link_floor)
+    #5️⃣ Create Floor (Different in API 21/22)
+    level_id = room.LevelId
+    new_floor = Floor.Create(doc, List_curve_loops, floor_type_id, level_id)
+
+
+    # Optionally: Change Parameter
+    offset_cm = 100
+    offset_ft = UnitUtils.ConvertToInternalUnits(offset_cm, UnitTypeId.Centimeters)
+
+    p_offset  = new_floor.get_Parameter(BuiltInParameter.FLOOR_HEIGHTABOVELEVEL_PARAM)
+    p_offset.Set(offset_ft)
+
+    #6️⃣ Create Report
+    room_name  = room.get_Parameter(BuiltInParameter.ROOM_NAME).AsString()
+    link_room  = output.linkify(room.Id, room_name)
+    link_floor = output.linkify(new_floor.Id, new_floor.Name)
+    level      = doc.GetElement(level_id)
+    area_m2    = UnitUtils.ConvertFromInternalUnits(room.Area, UnitTypeId.SquareMeters )
+
+    data = [link_floor, link_room, area_m2,level.Name, offset_cm]
+    table.append(data)
 
 t.Commit()  #🔒
+
+
+output.print_table(table_data=table,
+    title="Floorify Report",
+    columns=['Floor', 'Room', 'Area(m2)', 'Level', 'Offset (cm)'])
+
 #███████████████████████████████████████████████████████████████████████████
 # Happy Coding!
